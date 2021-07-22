5f1e63098032de1f5214b83849d24286fbccd78d
Matteo Bosco <matteobosco@alice.it>
2021-07-22 11:49:56 +0200

    gateway package

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM alpine:latest
+MAINTAINER woland7
+
+# Install Java
+RUN apk --no-cache add openjdk11-jre
+
+ENV JAVA_HOME /usr/lib/jvm/default-jvm
+
+# Set Unlimited Strength Jurisdiction Policy Files
+COPY UnlimitedJCEPolicyJDK8/ $JAVA_HOME/jre/lib/security/
+
+RUN mkdir /app \
+    && cd /app
+
+WORKDIR /app
+
+ADD SpringGateway-1.0.jar SpringGateway-1.0.jar
+
+ENV APP_HOME /app
+
+ENV PATH $APP_HOME/bin:$PATH
+
+EXPOSE 8081
+
+RUN chmod 777 -R /app
+
+CMD ["java", "-jar", "SpringGateway-1.0.jar"]
--- src/main/java/gateway/ApiGatewayApplication.java
+++ src/main/java/gateway/ApiGatewayApplication.java
@@ -1,3 +1,4 @@
+package gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,33 +7,31 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
-// tag::code[]
 @SpringBootApplication
 @RestController
-public class Application {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
-	// tag::route-locator[]
+	
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		String vehicleEndpoint = "http://" + System.getenv("VEHICLE_HOST") + ":8080";
-		String passengerEndpoint = "http://" + System.getenv("PASSENGER_HOST") + ":8080";
+		String vehicleEndpoint = "http://msvehicleservice-challenge1.router.default.svc.cluster.local";
+		String passengerEndpoint = "http://mspassengerservice-challenge1.router.default.svc.cluster.local";
 		System.out.println(vehicleEndpoint);
 		System.out.println(passengerEndpoint);
 		RouteLocator rl = builder.routes()
 			.route(p -> p
-				.path("/api-v1/ob/vehicles/**")
-				.filters(f -> f.rewritePath("/api-v1/ob/","/Vehicle-1.0/api-v1/"))
+				.path("/optimusbus/vehicles/**")
+				.filters(f -> f.rewritePath("/optimusbus/","/MSVehicle/Vehicle-1.0/"))
 				.uri(vehicleEndpoint))
 			.route(p -> p
-				.path("/api-v1/ob/passengers/**")
-				.filters(f -> f.rewritePath("/api-v1/ob/","/Passenger-1.0/api-v1/"))
+				.path("/optimusbus/passengers/**")
+				.filters(f -> f.rewritePath("/optimusbus/","/MSPassenger/Passenger-1.0/"))
 				.uri(passengerEndpoint))
 			.build();
 		return rl;
 	}
-	// end::route-locator[]
+	
 }
