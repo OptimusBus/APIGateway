a2ef038121004129ed3170a5edfb97a495306a50
Matteo Bosco <matteobosco@alice.it>
2021-11-13 19:29:27 +0100

    gateway update

--- src/main/java/gateway/ApiGatewayApplication.java
+++ src/main/java/gateway/ApiGatewayApplication.java
@@ -1,5 +1,7 @@
 package gateway;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -7,9 +9,12 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class ApiGatewayApplication {
+	
+	public ApiGatewayApplication() {}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
@@ -17,12 +22,15 @@ public class ApiGatewayApplication {
 	
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		
 		String vehicleEndpoint = "http://vehicle-optimusbus.router.default.svc.cluster.local/";
 		String passengerEndpoint = "http://passenger-optimusbus.router.default.svc.cluster.local/";
 		String routeEndpoint = "http://route-optimusbus.router.default.svc.cluster.local/";
 		String roadnetworkEndpoint = "http://roadnetwork-optimusbus.router.default.svc.cluster.local/";
 		String securityEndpoint = "http://security-optimusbus.router.default.svc.cluster.local/";
 		String bookingsEndpoint = "http://bookings-optimusbus.router.default.svc.cluster.local/";
+		String clientEndpoint = "http://client-optimusbus.router.default.svc.cluster.local/";
+		
 		RouteLocator rl = builder.routes()
 			.route(p -> p
 				.path("/optimusbus/vehicles/**")
@@ -48,6 +56,10 @@ public class ApiGatewayApplication {
 				.path("/optimusbus/bookings/**")
 				.filters(f -> f.rewritePath("/optimusbus/","/MSBookings/Bookings-1.0/"))
 				.uri(bookingsEndpoint))
+			.route(p -> p
+				.path("/optimusbus/VehicleClient/**")
+				.filters(f -> f.rewritePath("/optimusbus/",""))
+				.uri(clientEndpoint))
 			.build();
 		return rl;
 	}
